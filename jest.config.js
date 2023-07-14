210ef02e74b6a80e97475719606393967090808a
Rafael Martins <rafaelgemelli1@hotmail.com>
2023-07-14 14:28:45 -0400

    ⚡Projeto Finalizado

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  bail: true,
+  // o bail vem desligado por padrão, se um teste falhar, ele para de executar os testes
+  // se o bail não tiver como true, se um teste falhar, ele continua executando os outros
+  coverageProvider: "v8",
+
+  testMatch: [
+    "<rootDir>/src/**/*.spec.js",
+    "<rootDir>/*.spec.js", // expressão regular para o padrão dos arquivos de teste
+    // dentro de qualquer pasta, um arquivo com qualquer nome, cuja extensão seja .spec.js
+    // ignorar a pasta node_modules, consequentemente os testes rodam mais rápido
+    // <rootDir>/src/ para partir da raiz do projeto, dentro da pasta src
+  ],
+};
